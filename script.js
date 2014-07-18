a7d291b4d3d23288143cc2d6ae97282ed276e3b6
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-07-18 16:22:41 +0530

    Made everything look pretty and added some basic page layout stuff

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-$.get('config.json', function (data) {
-	console.log(data);
+$(document).ready(function () {
+	// constants
+	var CONFIG_DATA = {};
+	var REPO_DATA = {};
+	var GITHUB_API_BASE_URL = 'https://api.github.com/users/'; // trailing backslash required
+
+	$('#content').hide();
+
+	$.get('config.json', function (data) {
+		CONFIG_DATA = data;
+		setPageData();
+		$.get(GITHUB_API_BASE_URL + CONFIG_DATA.username + '/repos', function (repodata) {
+			REPO_DATA = repodata;
+			startPopulating();
+		});
+	});
+
+	function setPageData() {
+		$('#forkmebanner').attr('href', 'http://github.com/' + CONFIG_DATA.username);
+		$('#title').text(CONFIG_DATA.name + '\'s GitHub Portfolio');
+	}
+
+	function startPopulating () {
+		// do this last, after everything else has been added
+		$('#spinner').fadeOut(750, function () {
+			$('#content').fadeIn(500);
+		});
+	}
 });
